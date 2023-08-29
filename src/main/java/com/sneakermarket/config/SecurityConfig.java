f16d7a3ae8d80b86e0483757a42a10deff6fef13
reeHW <hjkh0107@gmail.com>
2023-08-29 14:07:52 +0900

    SecurityConfig 수정

--- src/main/java/com/sneakermarket/config/SecurityConfig.java
+++ src/main/java/com/sneakermarket/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .formLogin()
                     .loginPage("/auth/login")
                     .loginProcessingUrl("/auth/loginProc")
-                    .defaultSuccessUrl("/", true)
+                    .defaultSuccessUrl("http://ec2-3-34-83-230.ap-northeast-2.compute.amazonaws.com/", true)
                 .and()
                     .logout()
                         .logoutSuccessUrl("/")
