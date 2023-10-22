af7d4775d9ef88fffdb2d3eb5eb1fe8537930e7d
reeHW <hjkh0107@gmail.com>
2023-10-22 20:44:41 +0900

    log 설정 변경

--- src/main/java/com/sneakermarket/MarketApplication.java
+++ src/main/java/com/sneakermarket/MarketApplication.java
@@ -2,15 +2,12 @@ package com.sneakermarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class MarketApplication {
 
-	public static void main(String[] args) {;
-		SpringApplication application = new SpringApplication(MarketApplication.class);
-		application.addListeners(new ApplicationPidFileWriter());
-		application.run(args);
+	public static void main(String[] args) {
+		SpringApplication.run(MarketApplication.class, args);
 	}
 
 }
