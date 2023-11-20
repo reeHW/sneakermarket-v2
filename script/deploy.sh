800f9aada12dbb9028d9a391303fa3fde8b12727
reeHW <hjkh0107@gmail.com>
2023-11-20 11:33:53 +0900

    deploy.sh CURRENT_PID 탐색하지 못하던 문제 수정

--- script/deploy.sh
+++ script/deploy.sh
@@ -20,7 +20,7 @@ else
 fi
 
 echo "> 새 애플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | grep -v "plain" | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.java | grep -v "plain" | tail -n 1)
 
 echo "> JAR_NAME: $JAR_NAME"
 echo "> $JAR_NAME 에 실행권한 추가"
