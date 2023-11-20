2ac0e6e7c199d89d74140ed4ac35fceb95e926f9
reeHW <hjkh0107@gmail.com>
2023-11-20 10:34:17 +0900

    deploy.sh CURRENT_PID 탐색하지 못하던 문제 수정

--- script/deploy.sh
+++ script/deploy.sh
@@ -7,7 +7,7 @@ echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -f $PROJECT_NAME | grep java | awk '{print $1}')
+CURRENT_PID=$(pgrep -f1 $PROJECT_NAME | grep jar | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
@@ -16,6 +16,7 @@ if [ -z "$CURRENT_PID" ]; then
 else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
+  sleep 5
 fi
 
 echo "> 새 애플리케이션 배포"
